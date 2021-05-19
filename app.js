86712f685d8759637dbfae5f05302297f50f410a
moonzpwr <60944420+moonzpwr@users.noreply.github.com>
2021-05-19 17:01:53 +0300

    123

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
+<<<<<<< Updated upstream
 app.use(cors())
+=======
+app.use(express.static('public'));
+app.use(cors({
+  origin: '*',
+  methods: 'GET, HEAD, PUT,  PATCH, POST, DELETE, OPTIONS',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}))
+>>>>>>> Stashed changes
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
---
+++ model/users.js
@@ -0,0 +1,30 @@
+const User = require('./schemas/users')
+
+const findByID = async (id) => {
+    return await User.findOne({_id: id})
+}
+
+const findByEmail = async (email) => {
+    return await User.findOne({email})
+}
+
+const create = async (userOptions) => {
+    const user = new User(userOptions)
+    return await user.save()
+}
+
+const updateToken = async (id, token) => {
+    return await User.updateOne({_id: id}, {token})
+}
+
+const updateAvatar = async (id, avatar) => {
+    return await User.updateOne({_id: id}, {avatar})
+}
+
+module.exports = {
+    findByID,
+    findByEmail,
+    create,
+    updateToken,
+    updateAvatar
+}
